3ecaf7a8f9679a72e42cabb361dc011daf178b36
Celyne <dianakydd2@gmail.com>
2024-09-07 20:18:08 +0300

    updated bastion

--- terraform/modules/Autoscaling/lt-bastion-nginx.tf
+++ terraform/modules/Autoscaling/lt-bastion-nginx.tf
@@ -36,7 +36,7 @@ resource "aws_launch_template" "bastion-launch-template" {
 
 resource "aws_launch_template" "nginx-launch-template" {
   image_id               = var.ami-nginx
-  instance_type          = "t3.medium"
+  instance_type          = "t2.medium"
   vpc_security_group_ids = var.nginx-sg
 
   iam_instance_profile {
